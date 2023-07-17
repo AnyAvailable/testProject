f7c91ea2a3e410c49a266e9fcf465a1daa817a22
AnyAvailable <105864269+AnyAvailable@users.noreply.github.com>
2023-07-17 14:15:57 +0300

    Delete testProject directory

--- testProject/source code/clipp.py
+++
@@ -1,8 +0,0 @@
-#/usr/bin/env python
-from packetparcer import getbranches, getpackageandarchlists, packageandarchparce # importing functions
-"""calling the getbranches function inside getpackageandarchlists 
-inside the packageandarchparce function in general 
-this starts the module"""
-packageandarchparce(getpackageandarchlists(getbranches())) 
-
-
